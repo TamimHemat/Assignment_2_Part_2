0e6916f3fa2e3d8d05bb37225bdb3448c1334a8e
Tamim <tamimyasuo@gmail.com>
2021-11-30 22:14:31 -0800

    Added the previous code and added two new functions - CreateSingleIndex and getContactName

--- script.js
+++ script.js
@@ -1,93 +1,25 @@
-/* This is the code from Part 1 */
-
 const contactList = [  
 	{ 
 		name: "Raquel Ervin", 
 		phone: "+1 555 555-5555", 
 		address: "123 front st, Unit #1, Dakota City",    
-		email: "rocket@gmail.com",  
+		email: "rocket@gmail.com"
 	},   
-	{    
-		name: "Contact Name",    
-		phone: "Contact Phone",    
-		address: "Contact Address",    
-		email: "Contact Email",  
-	}
+    {	
+        name: "Jeremy McNinch",    
+        phone: "604 718 1043",    
+        address: "8631 Granville St, Vancouver",    
+        email: "email@email.com"
+    }
 ];
 
-function insertDOMIndex(contact) {
+function insertIndexPageDOM(contact) {
     return `
         <a href="page3.html"><div class="contact">${contact}</div></a>
     `
 }
 
-function cleanUpIndex(){
-    const clean = document.querySelectorAll('div.contact')
-    for (i=0; i<clean.length; i++){
-        clean[i].remove()
-    }
-}
-
-function createSingleIndex(contact){
-    const section = document.querySelector('.main')
-    section.insertAdjacentHTML('beforeend', insertDOMIndex(contact.name))
-}
-
-function renderIndex(contactList){
-    const section = document.querySelector('.main')
-    for (var obj of contactList){
-        section.insertAdjacentHTML('afterbegin', insertDOMIndex(obj.name))
-    }
-}
-
-function insertDOMCreate(contact = contactList[1]){
-    return `
-    <div class="contactedit">
-    <div class="contactimg">
-        <img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
-    </div>
-    <div class="form">
-        <form>
-            <div class="inputcontainer">
-                <input type="text" id="contactname" name="contactname" placeholder='${contact.name}'>
-                <button class="extrafield" id="extranamefield" name="extranamefield">+</button>
-            </div>
-            <div class="inputcontainer">
-                <input type="tel" id="contactphone" name="contactphone" placeholder="${contact.phone}">
-                <button class="extrafield" id="extraphonefield" name="extraphonefield">+</button>
-            </div>
-            <div class="inputcontainer">
-                <input type="text" id="contactaddress" name="contactaddress" placeholder="${contact.address}">
-                <button class="extrafield" id="extraaddressfield" name="extraaddressfield">+</button>
-            </div>
-            
-            <div class="inputcontainer">
-                <input type="email" id="contactemail" name="contactemail" placeholder="${contact.email}">
-                <button class="extrafield" id="extraemailfield" name="extraemailfield">+</button>
-            </div>
-            <div class="buttons">
-                <button type="submit" class="button save" id="savecontact" name="savecontact">Save Contact</button>
-                <button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
-            </div>
-        </form>
-    </div>
-    </div>
-    
-    `
-}
-
-function cleanUpCreate(){
-    const clean = document.querySelector('div.contactedit')
-    clean.remove()
-}
-
-function renderCreate(){
-    const section = document.querySelector('.main')
-    section.insertAdjacentHTML('afterbegin',insertDOMCreate())
-}
-
-
-function insertDOMView(contact){
+function insertViewPageDOM(contact){
     return `
     <div class="contactinfo">
             <div class="contactname">
@@ -105,15 +37,57 @@ function insertDOMView(contact){
     `
 }
 
-function cleanUpView(){
-    const clean = document.querySelector('div.contactinfo')
-    clean.remove()
+function insertCreatePageDOM(){
+    return `
+    <div class="contactedit">
+    <div class="contactimg">
+        <img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
+    </div>
+    <div class="form">
+        <form>
+            <div class="inputcontainer">
+                <input type="text" id="contactname" name="contactname" placeholder='Contact Name'>
+                <button class="extrafield" id="extranamefield" name="extranamefield">+</button>
+            </div>
+            <div class="inputcontainer">
+                <input type="tel" id="contactphone" name="contactphone" placeholder="Contact Phone">
+                <button class="extrafield" id="extraphonefield" name="extraphonefield">+</button>
+            </div>
+            <div class="inputcontainer">
+                <input type="text" id="contactaddress" name="contactaddress" placeholder="Contact Address">
+                <button class="extrafield" id="extraaddressfield" name="extraaddressfield">+</button>
+            </div>
+            
+            <div class="inputcontainer">
+                <input type="email" id="contactemail" name="contactemail" placeholder="Contact Email">
+                <button class="extrafield" id="extraemailfield" name="extraemailfield">+</button>
+            </div>
+            <div class="buttons">
+                <button type="submit" class="button save" id="savecontact" name="savecontact">Save Contact</button>
+                <button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
+            </div>
+        </form>
+    </div>
+    </div>
+    
+    `
 }
 
-function renderView(contact){
-    const section = document.querySelector('.main')
-    section.insertAdjacentHTML("afterbegin",insertDOMView(contact))
+function getContactName(search){
+    for(let names in contactList){
+        if(search == contactList[names].name){
+            return contactList[names]
+        }
+    }
+    alert("Contact not found")
 }
 
-/* Now starts the code for Part 2 */
+
+function createSingleIndex(evt){
+    let search = evt.target.textContent
+    let cName = getContactName(search)
+    if (cName !=null){
+        cleanUpIndex()
+        renderView(cName)
+    }}
 
